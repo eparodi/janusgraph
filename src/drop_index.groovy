d3df7e1a1b4534590ae9c89559b81838b70f11a1
Eliseo Parodi Almaraz <eparodi@users.noreply.github.com>
2019-11-26 23:41:19 -0300

    Update drop_index.groovy

--- src/drop_index.groovy
+++ src/drop_index.groovy
@@ -3,5 +3,12 @@ def dropIndex(graph, indexName) {
     idx = mgmt.getGraphIndex(indexName)
     mgmt.updateIndex(idx), SchemaAction.DISABLE_INDEX).get()
     mgmt.commit()
+    graph.tx().commit()
     ManagementSystem.awaitGraphIndexStatus(graph, indexName).statis(SchemaStatus.DISABLED).call()
+
+    mgmt = graph.openManagement()
+    delIndex = mgmt.getGraphIndex(indexName)
+    mgmt.updateIndex(delIndex, SchemaAction.REMOVE_INDEX)
+    mgmt.commit()
+    graph.tx().commit()
 }
